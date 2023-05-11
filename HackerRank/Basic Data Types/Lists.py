3bd406abae0c830420900b313b8b5a9bd316bad4
unknown <raviyaadhav@gmail.com>
2023-05-11 19:14:37 +0530

    Updated Commit

--- HackerRank/Basic Data Types/Lists.py
+++ HackerRank/Basic Data Types/Lists.py
@@ -6,14 +6,16 @@ if __name__ == '__main__':
         s=input().split()
 
         if s[0]=='insert':
-            ans.insert(s[2],s[1])
+            ans.insert(int(s[1]),int(s[2]))
         if s[0]=='print':
             print(ans)
         if s[0]=='sort':
             ans=sorted(ans)
         if s[0]=='append':
-            ans.append(s[1])
+            ans.append(int(s[1]))
         if s[0]=='pop':
             ans.pop()
+        if s[0]=='remove':
+            ans.remove(int(s[1]))
         if s[0]=='reverse':
             ans=ans[::-1]
